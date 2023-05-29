36a063b8af7e992b6c77fdb6ec2deaf49cfe55eb
pablo023 <117492207+pablo023@users.noreply.github.com>
2023-05-29 16:28:47 +0200

    ISSUE-4: Include Checkstyle plugin into Build (#15)
    ISSUE-4: Create Dockerfile & Set-up Docker-Compose
    
    ISSUE-4: Set up GitHub Actions
    
    Bugfixes:
    
    - Fix image name in deploy-docker-image.yml
    - Fix deploy-artifact.yml
    - Add distributionManagement in pom.xml
    
    - Remove settings.xml & fix docker image name

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM openjdk:20
+ADD target/user-profile-service-0.0.1-SNAPSHOT.jar app.jar
+EXPOSE 8080
+EXPOSE 8443
+CMD ["java", "-jar", "app.jar"]
--- src/main/java/com/iprody/userprofile/UserProfileServiceApplication.java
+++ src/main/java/com/iprody/userprofile/UserProfileServiceApplication.java
@@ -1,13 +1,17 @@
-package com.iprody.userprofile.profileservice;
+package com.iprody.userprofile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
 import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
 
+
 @SpringBootApplication(exclude = {R2dbcAutoConfiguration.class, LiquibaseAutoConfiguration.class})
 public class UserProfileServiceApplication {
 
+    /**
+     * @param args The command-line arguments passed to the program
+     */
     public static void main(String[] args) {
         SpringApplication.run(UserProfileServiceApplication.class, args);
     }
