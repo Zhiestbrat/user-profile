ecb43e6a5720902a6a4bc68902ee6313e057500b
pablo023 <117492207+pablo023@users.noreply.github.com>
2023-06-01 16:57:05 +0200

    Feature/18 up logging distributed tracing (#23)
    * ISSUE-18: Logging & Distributed tracing
    
    - Add log4j2 and micrometer tracer
    - Add rules for tracing in `application.yml`
    - Add log4j2 configs
    - Add dependencies for micrometer and zipkin brave pkg
    - Add logger & tracer into UserProfileServiceApplication
    - Restructure pom.xml
    -- Now dependencies are grouped by their purpose and separated by comments
    - Set Rolling Policy to Daily

--- src/main/java/com/iprody/userprofile/UserProfileServiceApplication.java
+++ src/main/java/com/iprody/userprofile/UserProfileServiceApplication.java
@@ -1,5 +1,10 @@
 package com.iprody.userprofile;
 
+import brave.Tracer;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
@@ -7,13 +12,19 @@ import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
 
 
 @SpringBootApplication(exclude = {R2dbcAutoConfiguration.class, LiquibaseAutoConfiguration.class})
+@Slf4j
+@RequiredArgsConstructor
 public class UserProfileServiceApplication {
+
+    private static final Logger LOGGER = LogManager.getLogger(UserProfileServiceApplication.class);
+
+    private final Tracer tracer;
 
     /**
      * @param args The command-line arguments passed to the program
      */
     public static void main(String[] args) {
+
         SpringApplication.run(UserProfileServiceApplication.class, args);
     }
-
 }
