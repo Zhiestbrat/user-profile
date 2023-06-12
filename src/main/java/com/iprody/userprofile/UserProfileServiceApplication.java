223d7396c71e740e54150dc491d377703ecc6d70
pablo023 <117492207+pablo023@users.noreply.github.com>
2023-06-12 16:11:57 +0200

    Fix JDBC error / PSQLException: Connection to localhost:5432 (#35)
    - Update datasource URL
    - Add environment to UP-service docker compose
    
    ---------------==========---------------
    
    - Conflict Resolved
    
    Co-authored-by: Pavel Voedilov <voedilov.p@gmail.com>

--- src/main/java/com/iprody/userprofile/UserProfileServiceApplication.java
+++ src/main/java/com/iprody/userprofile/UserProfileServiceApplication.java
@@ -1,7 +1,5 @@
 package com.iprody.userprofile;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
@@ -9,10 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
 import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
 
-
 @SpringBootApplication(exclude = {R2dbcAutoConfiguration.class, LiquibaseAutoConfiguration.class})
-@Slf4j
-@RequiredArgsConstructor
 public class UserProfileServiceApplication {
 
     private static final Logger LOGGER = LogManager.getLogger(UserProfileServiceApplication.class);
@@ -21,7 +16,7 @@ public class UserProfileServiceApplication {
      * @param args The command-line arguments passed to the program
      */
     public static void main(String[] args) {
-
         SpringApplication.run(UserProfileServiceApplication.class, args);
     }
+
 }
